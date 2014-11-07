50797f424436978a5b0d2be989283e96176d3f44
goffer123 <goffersoft@gmail.com>
2014-11-06 20:04:41 -0500

    refacter

--- src/com/goffersoft/core/object.h
+++ src/com/goffersoft/core/object.h
@@ -1,3 +1,23 @@
+/** 
+ **
+ ** This file is part of goffersoft.
+ **
+ ** goffersoft is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** goffersoft is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with goffersoft. If not, see <http://www.gnu.org/licenses/>.
+ **
+ ** This file descrbes the object class of the goffersoft framework. 
+ ** This class is the root class of all other classes.
+ **/
 #ifndef __OBJECT_H__
 #define __OBJECT_H__
 
--- src/test/com/goffersoft/core/object.cpp
+++ src/test/com/goffersoft/core/object.cpp
@@ -1,3 +1,22 @@
+/**
+ **
+ ** This file is part of goffersoft.
+ **
+ ** goffersoft is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** goffersoft is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with goffersoft. If not, see <http://www.gnu.org/licenses/>.
+ ** 
+ ** This file contains test code for the object.h class
+ **/
 #include "object.h"
 #include "iostream"
 
@@ -78,9 +97,9 @@ int main (int argc, char **argv) {
     Object *obj4 = &b2;
     Object *obj5 = &c1;
 
-    cout << (*obj1 == *obj2) << endl;
-    cout << (*obj3 == *obj4) << endl;
-    cout << (*obj1 == *obj4) << endl;
-    cout << (*obj3 == *obj1) << endl;
-    cout << (*obj5 == *obj1) << endl;
+    cout << string(*obj1) << " " << (*obj1 == *obj2) << endl;
+    cout << string(*obj2) << " " << (*obj3 == *obj4) << endl;
+    cout << string(*obj3) << " " << (*obj1 == *obj4) << endl;
+    cout << string(*obj4) << " " << (*obj3 == *obj1) << endl;
+    cout << string(*obj5) << " " << (*obj5 == *obj1) << endl;
 }
