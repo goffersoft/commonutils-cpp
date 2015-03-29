9879810e14617bcff5c4a8cf41a04c9ee9b63b7f
goffer123 <peaswar@gmail.com>
2015-03-28 23:09:24 -0400

    add switch to optionally link boost libs

--- experimental/src/com/goffersoft/core/object.h
+++ experimental/src/com/goffersoft/core/object.h
@@ -1,21 +1,21 @@
 /** 
  **
- ** This file is part of mepgutils.
+ ** This file is part of experimental.
  **
- ** mpegutils is free software: you can redistribute it and/or modify
+ ** experimental is free software: you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
  ** the Free Software Foundation, either version 3 of the License, or
  ** (at your option) any later version.
  **
- ** mpegutils is distributed in the hope that it will be useful,
+ ** experimental is distributed in the hope that it will be useful,
  ** but WITHOUT ANY WARRANTY; without even the implied warranty of
  ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  ** GNU General Public License for more details.
  **
  ** You should have received a copy of the GNU General Public License
- ** along with mpegutils. If not, see <http://www.gnu.org/licenses/>.
+ ** along with experimental. If not, see <http://www.gnu.org/licenses/>.
  **
- ** This file descrbes the object class of the mpegutils project.
+ ** This file descrbes the object class of the experimental project.
  ** This class is the root class of all other classes.
  **/
 #ifndef __OBJECT_H__
