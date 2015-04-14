955e7b24abb0aebebac52319571ece4a6f8d546a
goffer123 <peaswar@gmail.com>
2015-04-13 22:10:38 -0400

    bugfixes

--- mpegutils/test/com/goffersoft/core/list.cc
+++ mpegutils/test/com/goffersoft/core/list.cc
@@ -1,19 +1,19 @@
 /**
  **
- ** This file is part of goffersoft.
+ ** This file is part of mpegutils.
  **
- ** goffersoft is free software: you can redistribute it and/or modify
+ ** mpegutils is free software: you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
  ** the Free Software Foundation, either version 3 of the License, or
  ** (at your option) any later version.
  **
- ** goffersoft is distributed in the hope that it will be useful,
+ ** mpegutils is distributed in the hope that it will be useful,
  ** but WITHOUT ANY WARRANTY; without even the implied warranty of
  ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  ** GNU General Public License for more details.
  **
  ** You should have received a copy of the GNU General Public License
- ** along with goffersoft. If not, see <http://www.gnu.org/licenses/>.
+ ** along with mpegutils. If not, see <http://www.gnu.org/licenses/>.
  ** 
  ** This file contains test code for the list.h class
  **/
