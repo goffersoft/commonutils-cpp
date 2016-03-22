339d83709674f3b40b449b514702304fceba1e10
goffer123 <goffersoft@gmail.com>
2016-03-22 13:15:49 -0400

    cleanup comments

--- experimental/src/com/goffersoft/core/utils.h
+++ experimental/src/com/goffersoft/core/utils.h
@@ -1,21 +1,21 @@
 /** 
  **
- ** This file is part of algs4cc.
+ ** This file is part of experimental.
  **
- ** algs4cc is free software: you can redistribute it and/or modify
+ ** experimental is free software: you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
  ** the Free Software Foundation, either version 3 of the License, or
  ** (at your option) any later version.
  **
- ** algs4cc is distributed in the hope that it will be useful,
+ ** experimental is distributed in the hope that it will be useful,
  ** but WITHOUT ANY WARRANTY; without even the implied warranty of
  ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  ** GNU General Public License for more details.
  **
  ** You should have received a copy of the GNU General Public License
- ** along with algs4cc. If not, see <http://www.gnu.org/licenses/>.
+ ** along with experimental. If not, see <http://www.gnu.org/licenses/>.
  **
- ** This file descrbes the utils class of the algs4cc project.
+ ** This file descrbes the utils class of the experimental project.
  ** This class cannot be instantiated. Has a bunch of static
  ** helper functions
  **/
--- template/src/com/goffersoft/core/except.h
+++ template/src/com/goffersoft/core/except.h
@@ -1,22 +1,22 @@
 /** 
  **
- ** This file is part of algs4cc.
+ ** This file is part of template.
  **
- ** algs4cc is free software: you can redistribute it and/or modify
+ ** template is free software: you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
  ** the Free Software Foundation, either version 3 of the License, or
  ** (at your option) any later version.
  **
- ** algs4cc is distributed in the hope that it will be useful,
+ ** template is distributed in the hope that it will be useful,
  ** but WITHOUT ANY WARRANTY; without even the implied warranty of
  ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  ** GNU General Public License for more details.
  **
  ** You should have received a copy of the GNU General Public License
- ** along with algs4cc. If not, see <http://www.gnu.org/licenses/>.
+ ** along with template. If not, see <http://www.gnu.org/licenses/>.
  **
  ** This file descrbes the various exception classes of the
- ** algs4cc project.
+ ** template project.
  **      not_implmeneted_error exception class
  **/
 #ifndef __EXCEPT_H__
